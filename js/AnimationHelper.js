946c63718d7de24fe6edc4fe1f0a8084f782e64e
Poletipo <tijimjim@hotmail.com>
2020-10-31 19:54:53 -0400

    Animation helper marche a peu pres

--- js/AnimationHelper.js
+++ js/AnimationHelper.js
@@ -1,9 +1,9 @@
-export default class AnimationaHelper{
+export default class AnimationHelper{
     constructor(node, animationKeys){
         //[x = 0,y = 1,finalSpeed = 2, time = 3]
         this.node = node;
-        this.animKey = [[50,50, 0,0],[1000,800,500,5],[500,500,0,2]];
-        //this.animKey = animationKeys;
+        //this.animKey = [[0,0, 0,0],[783,0, 0,3]];//,[200,0,500,2],[0,500,0,2],[50,50, 0,1],[783,800,10,1]];
+        this.animKey = animationKeys;
         this.speedInitX = 0;
         this.speedInitY = 0;
         this.accX = 0;
@@ -12,16 +12,47 @@ export default class AnimationaHelper{
         this.displY = 0;
         this.timer = 0;
 
+        this.node.style.left = this.animKey[0][0] + "px";
+
         this.isAnim = false;
         this.index = 1;
     }
 
     tick(deltatick){
+        let alive = true;
+        if(!this.isAnim){
+            let dist = this.animKey[this.index][0] - this.animKey[(this.index-1)][0];
+            console.log(dist);
+            this.speedInitX = ((dist*2)/this.animKey[this.index][3])-this.animKey[this.index][2];
+            //this.speedInitX = this.speedInitX < 0 ? 0 : this.speedInitX;
+            this.accX = (this.animKey[this.index][2]-this.speedInitX)/this.animKey[this.index][3];
 
-        this.speedInitX = (((this.animKey[this.index][0] - this.animKey[(this.index-1)][0])*2)/this.animKey[this.index][3])-this.animKey[this.index][2];
+            this.isAnim = true;
+        }
 
-        this.accX = (this.animKey[this.index][2]-this.speedInitX)/this.animKey[this.index][3];
+        this.timer += deltatick;
+        if (this.timer <= this.animKey[this.index][3]){
+            this.displX = (this.speedInitX*this.timer) + ((this.accX * Math.pow(this.timer,2))/2);
+            //console.log(this.displX);
+            let x = this.animKey[(this.index - 1)][0] + this.displX;
+            this.node.style.left = x + "px";
 
+            console.log("Index: " + this.index, this.speedInitX, this.accX, "Displ: " +this.displX, this.timer);
+
+        }
+        else{
+            this.timer = 0;
+            this.index++;
+            this.isAnim = false;
+            if(this.index >= this.animKey.length){
+                alive = false;
+            }
+        }
+
+
+        
+
+        return alive;
     }
 
 
--- js/sprite/Geralt.js
+++ js/sprite/Geralt.js
@@ -1,45 +1,25 @@
-
+import AnimationHelper from "../AnimationHelper.js"
 export default class Geralt{
     constructor(){
-        
-        this.posInit = [-783,0,0]; //[x,y,speed]
-        this.posFinal = [0,0,0];
-        this.x = this.posInit[0];
-        this.y = this.posInit[1];
-        this.timeToAnim = 2;
-
-        this.posInit[2] = (((this.posFinal[0] - this.posInit[0])*2)/this.timeToAnim)-this.posFinal[2];
-
-        this.speed = this.posInit[2];
-
-        this.acceleration = (this.posFinal[2]-this.speed)/this.timeToAnim;
-
-        this.timer = 0;
-        this.displacement = 0;
 
         this.node = document.querySelector(".geralt");
 
-        this.node.style.left = this.x + "px";
-        this.node.style.top = this.y + "px";
+        this.anim = new AnimationHelper(this.node,[[-783,0, 0,0],[0,0, 0,3]]);
+
     }
 
 
     tick(deltatick){
         
         let alive = true;
-        this.timer += deltatick;
-        if (this.timer <= this.timeToAnim){
-            this.displacement = this.speed*this.timer + ((this.acceleration * Math.pow(this.timer,2))/2);
+        let aliveAnim = false;
+        if(this.anim){
+            aliveAnim = this.anim.tick(deltatick);
         }
-        else{
-            alive = false;
+        if(!aliveAnim){
+            this.anim = null;
         }
 
-        this.x = this.posInit[0] + this.displacement;
-        console.log(this.node.style.left);
-
-        this.node.style.left = this.x + "px";
-    
         return alive;
     }
 
