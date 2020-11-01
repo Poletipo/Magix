4fb62d4c7c5f86fc54cd07091a221e95938f2b46
Poletipo <tijimjim@hotmail.com>
2020-11-01 12:17:43 -0500

    Title anim

--- js/sprite/Title.js
+++ js/sprite/Title.js
@@ -3,18 +3,32 @@ export default class Title{
     constructor(){
 
         this.node = document.querySelector(".login-title");
-        this.animKeys = [[0,0,0,0],[0,500,1000,2]];
+        this.animKeys = [[-500,-200,0,0],[0,450,10000,1]];
         this.anim = new AnimationHelper(this.node,this.animKeys);
+        this.timer = 0;
+        this.sizeX = 200;
+        this.sizeY = 150;
+        this.dimX = 100;
+        this.dimY = 100;
 
-
+        this.node.style.width = 200 + "%";
+        //this.node.style.width = 5000 + "px";
+        this.node.style.height = 200 + "%";
+        
     }
-
+    
     tick(deltatick){
         
         let alive = true;
         let aliveAnim = false;
         if(this.anim){
             aliveAnim = this.anim.tick(deltatick);
+            
+            this.sizeX -= this.dimX * deltatick;
+            this.sizeY -= this.dimY * deltatick;
+            this.node.style.width = this.sizeX + "%";
+            this.node.style.height = this.sizeY + "%";
+            
         }
         if(!aliveAnim){
             this.anim = null;
