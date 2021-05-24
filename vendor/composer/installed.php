e3f8819c03bf3c3392718bdc1223f89d27aacddd
Poletipo <tijimjim@hotmail.com>
2021-05-23 22:31:44 -0400

    added composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd5a4f2c52938931a52961be1f4817209d6ce7e78',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd5a4f2c52938931a52961be1f4817209d6ce7e78',
+    ),
+    'heroku/heroku-buildpack-php' => 
+    array (
+      'pretty_version' => 'v192',
+      'version' => '192.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '360fa3ee3caad234760048da8bc8fefd35609ea9',
+    ),
+  ),
+);
